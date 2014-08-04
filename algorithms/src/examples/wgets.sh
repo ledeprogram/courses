78298624258b709f4c70f85fceadad0b8cdb6c34
chrishwiggins <chris@hackny.org>
2014-08-04 11:33:40 -0400

    added gmm plotting thingy

--- algorithms/src/examples/wgets.sh
+++ algorithms/src/examples/wgets.sh
@@ -1,3 +1,4 @@
+wget http://scikit-learn.org/stable/_downloads/plot_gmm_pdf.py
 wget http://scikit-learn.org/stable/_downloads/plot_gmm_classifier.py
 wget http://scikit-learn.org/0.11/_downloads/plot_iris_classifiers.py
 wget http://scikit-learn.org/stable/_downloads/plot_underfitting_overfitting.py
